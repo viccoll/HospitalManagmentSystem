edeae21564c2bee2292098f377d514d6b9499814
qwetqywer <lifika953@gmail.com>
2020-11-30 03:02:23 +0300

    feat:add 2 fxml files

---
+++ HospitalManageSystem/src/Controllers/AdministratorAccountController.java
@@ -0,0 +1,58 @@
+package Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class AdministratorAccountController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button desktopAdministratorButton;
+
+    @FXML
+    private Button registryManagementButton;
+
+    @FXML
+    private Button personnelManagementButton;
+
+    @FXML
+    private Button makeWorkScheduleButton;
+
+    @FXML
+    private Button viewStatisticsButton;
+
+    @FXML
+    private Button settingsAdministratorButton;
+
+    @FXML
+    private Label fullNameAdministratorLabel;
+
+    @FXML
+    private Label surnameAdministratorLabel;
+
+    @FXML
+    private Label nameAdministratorLabel;
+
+    @FXML
+    private Label patronymicAdministratorLabel;
+
+    @FXML
+    private Label birthdayAdministratorLabel;
+
+    @FXML
+    private Label addressAdministratorLabel;
+
+    @FXML
+    void initialize() {
+
+    }
+}
+
